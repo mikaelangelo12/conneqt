c38adc0f2a285c53a9dde127b1005cac439f61b4
Mikael Angelo <mikaelangelo12@gmail.com>
2021-12-04 05:07:48 -0300

    Finishing the menuList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ViewsModule } from './views/views.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { ViewsModule } from './views/views.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    ViewsModule
+    ViewsModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [HomeComponent]
 })
